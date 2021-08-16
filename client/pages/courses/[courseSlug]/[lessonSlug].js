6e8a355842f8388f1cb45d755b1c76c79217694b
iamtu <trandinhtu.dev@gmail.com>
2021-08-16 11:45:59 -0400

    create nextjs page: auth, course detail, lesson detail

---
+++ client/pages/courses/[courseSlug]/[lessonSlug].js
@@ -0,0 +1,11 @@
+import React from "react";
+
+const LessonDetailPage = () => {
+  return (
+    <div>
+      <h1>Lesson Detail Page</h1>
+    </div>
+  );
+};
+
+export default LessonDetailPage;
---
+++ client/pages/courses/[courseSlug]/index.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+const CourseDetailPage = () => {
+  return (
+    <div>
+      <h1>Course Detail Page</h1>
+    </div>
+  );
+};
+
+export default CourseDetailPage;
