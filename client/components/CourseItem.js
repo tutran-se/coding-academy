843f624e2a759240d8610065e0d730ba875a8a2f
iamtu <trandinhtu.dev@gmail.com>
2021-08-16 12:58:23 -0400

    style and populate HomePage

---
+++ client/components/CourseItem.js
@@ -0,0 +1,69 @@
+import React from "react";
+import { Badge, Box, Stack } from "@chakra-ui/layout";
+import Link from "next/link";
+import NextImage from "next/image";
+import { Button } from "@chakra-ui/button";
+const CourseItem = ({ course }) => {
+  const { name, slug, summary, image, price, totalLessons, totalDurations } =
+    course;
+  return (
+    <Box
+      maxW={["full", "full", "48%", "32%"]}
+      borderRadius="lg"
+      overflow="hidden"
+      boxShadow="xl"
+      m="1.5"
+      borderWidth="thin"
+    >
+      <Link href={`/courses/${slug}`}>
+        <a>
+          <NextImage src={image.url} alt={"title"} width="786" height="443" />
+        </a>
+      </Link>
+
+      <Box p="6">
+        <Box d="flex" alignItems="center">
+          <Stack>
+            <Badge
+              borderRadius="base"
+              px="2"
+              colorScheme="red"
+              textDecor="line-through"
+            >
+              Price: ${price}
+            </Badge>
+            <Badge borderRadius="base" px="2" colorScheme="green">
+              Price: ${price}
+            </Badge>
+          </Stack>
+
+          <Box
+            color="gray.500"
+            fontWeight="semibold"
+            letterSpacing="wide"
+            fontSize="xs"
+            ml="2"
+          >
+            {totalLessons} lessons &bull; {totalDurations}h
+          </Box>
+        </Box>
+
+        <Box mt="1" fontWeight="bold" lineHeight="tight" fontSize="lg">
+          <Link href={`/courses/${slug}`}>
+            <a>{name}</a>
+          </Link>
+        </Box>
+        <Box color="gray.400">{summary}</Box>
+        <Box mt="3">
+          <Link href={`/courses/${slug}`}>
+            <a>
+              <Button colorScheme="orange"> 🔥 View Details</Button>
+            </a>
+          </Link>
+        </Box>
+      </Box>
+    </Box>
+  );
+};
+
+export default CourseItem;
--- client/pages/index.js
+++ client/pages/index.js
@@ -1,11 +1,24 @@
 import React from "react";
-
-const Homepage = () => {
+import CourseItem from "../components/CourseItem";
+import { Flex } from "@chakra-ui/layout";
+const Homepage = ({ result: courses }) => {
+  console.log(courses);
   return (
-    <div>
-      <h1>Hello</h1>
-    </div>
+    <Flex>
+      {courses.map((course) => (
+        <CourseItem course={course} key={course.id} />
+      ))}
+    </Flex>
   );
 };
+
+export async function getStaticProps(context) {
+  const response = await fetch(`${process.env.NEXT_PUBLIC_STRAPI_API}/courses`);
+  const result = await response.json();
+
+  return {
+    props: { result }, // will be passed to the page component as props
+  };
+}
 
 export default Homepage;
