e40d5c19e66937e28e6cfefe3c58f438986897f6
iamtu <trandinhtu.dev@gmail.com>
2021-08-16 10:47:15 -0400

    config Cloudinary

--- server/config/plugins.js
+++ server/config/plugins.js
@@ -10,4 +10,12 @@ module.exports = ({ env }) => ({
       testAddress: "trandinhtu.ktv@gmail.com",
     },
   },
+  upload: {
+    provider: "cloudinary",
+    providerOptions: {
+      cloud_name: env("CLOUDINARY_NAME"),
+      api_key: env("CLOUDINARY_KEY"),
+      api_secret: env("CLOUDINARY_SECRET"),
+    },
+  },
 });
